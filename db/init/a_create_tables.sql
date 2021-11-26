94e312ef5dc56a0888f5aaf644cdde865c4b0b09
shou-watanabe <watanabeshou1520@gmail.com>
2021-11-26 22:39:58 +0900

    :sparkles: #6 ガチャテーブル作成

--- db/init/a_create_tables.sql
+++ db/init/a_create_tables.sql
@@ -2,6 +2,17 @@ use go_database;
 
 create table users (
     id int unsigned primary key auto_increment,
-    name varchar(120) not null,
+    name varchar(60) not null,
     token varchar(36) not null unique
+);
+
+create table characters (
+    id int unsigned primary key auto_increment,
+    name varchar(60) not null,
+    rate float not null
+);
+
+create table user_charactors (
+    user_charactor_id int unsigned
+    charactor_id int unsigned
 );
