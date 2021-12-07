d25be9c94b3595d1022adff14d05022175b6554f
shou-watanabe <watanabeshou1520@gmail.com>
2021-12-07 22:32:57 +0900

    :wrench: #19 wireの導入

---
+++ src/wire.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/google/wire"
+	"github.com/jinzhu/gorm"
+
+	"techtrain-mission/src/infra"
+	"techtrain-mission/src/presen/handler"
+	"techtrain-mission/src/usecase"
+)
+
+func initUserHandler(db *gorm.DB) handler.UserHandler {
+	wire.Build(
+		infra.NewUserRepository,
+		usecase.NewUserUsecase,
+		handler.NewUserHandler,
+	)
+	return nil
+}
