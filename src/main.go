f3fd8eed8e98c08922696b1f288d2b1825b81cb7
shou-watanabe <watanabeshou1520@gmail.com>
2021-12-10 10:54:30 +0900

    :recycle: #27 mysqlの接続コードをsqlパッケージに移動

--- src/main.go
+++ src/main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 	"techtrain-mission/src/presen/handler"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
 )
 
 func main() {
-	db := sqlConnect()
-	defer db.Close()
+	userHandler := initUserHandler()
 
-	userHandler := initUserHandler(db)
-
-	// e := echo.New()
-	// e.Use(middleware.Logger)
 	handler.InitRouting(userHandler)
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -27,37 +19,4 @@ func main() {
 	if err := http.ListenAndServe(port, nil); err != nil {
 		panic(err)
 	}
-}
-
-func sqlConnect() (database *gorm.DB) {
-	DBMS := "mysql"
-	USER := "go_test"
-	PASS := "password"
-	PROTOCOL := "tcp(db:3306)"
-	DBNAME := "go_database"
-
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
-
-	count := 0
-	db, err := gorm.Open(DBMS, CONNECT)
-	if err != nil {
-		for {
-			if err == nil {
-				fmt.Println("")
-				break
-			}
-			fmt.Print(".")
-			time.Sleep(time.Second)
-			count++
-			if count > 180 {
-				fmt.Println("")
-				fmt.Println("DB connect failed")
-				panic(err)
-			}
-			db, err = gorm.Open(DBMS, CONNECT)
-		}
-	}
-	fmt.Println("DB connect success")
-
-	return db
 }
