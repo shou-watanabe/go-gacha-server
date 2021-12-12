85e6546e90070b0401c3931c3e573ddf839d8f5b
shou-watanabe <watanabeshou1520@gmail.com>
2021-12-12 18:21:59 +0900

    :recycle: #42 sql.DBを使うように変更

--- src/infra/repository/user.go
+++ src/infra/repository/user.go
@@ -1,44 +1,42 @@
 package repository
 
 import (
-	"errors"
+	"database/sql"
 	"techtrain-mission/src/domain/entity"
 	"techtrain-mission/src/domain/repository"
-
-	"github.com/jinzhu/gorm"
 )
 
 type userRepository struct {
-	Conn *gorm.DB
+	db *sql.DB
 }
 
-func NewUserRepository(conn *gorm.DB) repository.UserRepository {
-	return &userRepository{Conn: conn}
+func NewUserRepository(db *sql.DB) repository.UserRepository {
+	return &userRepository{db: db}
 }
 
 func (ur *userRepository) Create(name string) (*entity.User, error) {
 	ue := &entity.User{}
-	ur.Conn.Table("users").Exec("insert users (name, token) value (?, UUID())", name).Scan(&ue)
-	if ue.Name == "" {
-		err := errors.New("use not found")
-		return ue, err
-	}
+	// ur.Conn.Table("users").Exec("insert users (name, token) value (?, UUID())", name).Scan(&ue)
+	// if ue.Name == "" {
+	// 	err := errors.New("use not found")
+	// 	return ue, err
+	// }
 	return ue, nil
 }
 
 func (ur *userRepository) Get(token string) (*entity.User, error) {
 	user := &entity.User{}
-	if err := ur.Conn.Table("users").First(&user, "token = ?", token).Error; err != nil {
-		return nil, err
-	}
+	// if err := ur.Conn.Table("users").First(&user, "token = ?", token).Error; err != nil {
+	// 	return nil, err
+	// }
 
 	return user, nil
 }
 
 func (ur *userRepository) Update(name string, token string) (*entity.User, error) {
 	user := &entity.User{Token: token}
-	if err := ur.Conn.Table("users").Update("name", name).Error; err != nil {
-		return nil, err
-	}
+	// if err := ur.Conn.Table("users").Update("name", name).Error; err != nil {
+	// 	return nil, err
+	// }
 	return user, nil
 }
