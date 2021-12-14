5fce0f2a09444b673932cee4498aa687b7b42b33
shou-watanabe <watanabeshou1520@gmail.com>
2021-12-14 21:53:02 +0900

    :sparkles: #46 キャラクターの前取得追加

--- src/domain/repository/chara.go
+++ src/domain/repository/chara.go
@@ -1,5 +1,11 @@
 package repository
 
+import (
+	"context"
+
+	"techtrain-mission/src/domain/entity"
+)
+
 type CharaRepository interface {
-	//
+	List(ctx context.Context) ([]*entity.Chara, error)
 }
