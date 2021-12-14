527f3f872ed2f107bf2af8b2e4218c8ae0d87443
shou-watanabe <watanabeshou1520@gmail.com>
2021-12-14 13:53:30 +0900

    :recycle: #42 gormを使わずにUpdate

--- src/infra/repository/user.go
+++ src/infra/repository/user.go
@@ -90,10 +90,24 @@ func (ur *userRepository) Get(ctx context.Context, token string) (*entity.User, 
 }
 
 func (ur *userRepository) Update(ctx context.Context, name string, token string) (*entity.User, error) {
-	user := &entity.User{Token: token}
+	const update = `UPDATE users Set name = ? WHERE token = ?`
+
+	ue := &entity.User{}
+	stmt, err := ur.db.PrepareContext(ctx, update)
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, name, token)
+	if err != nil {
+		return nil, err
+	}
+
 	// gormバージョン
 	// if err := ur.Conn.Table("users").Update("name", name).Error; err != nil {
 	// 	return nil, err
 	// }
-	return user, nil
+	return ue, nil
 }
