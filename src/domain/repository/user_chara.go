3834e4eaeaa88aeb8f2d520aa7a7e34ab197079a
shou-watanabe <watanabeshou1520@gmail.com>
2021-12-14 22:28:19 +0900

    :sparkles: #46 ガチャの結果をキャラ保持テーブルに反映

--- src/domain/repository/user_chara.go
+++ src/domain/repository/user_chara.go
@@ -8,4 +8,5 @@ import (
 
 type UserCharaRepository interface {
 	List(ctx context.Context, ue entity.User) ([]*entity.UserChara, error)
+	Store(ctx context.Context, ue entity.User, ces []*entity.Chara) error
 }
