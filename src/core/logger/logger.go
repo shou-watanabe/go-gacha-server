cc0f2c1e67fb45a9ef2b77d1eb9b91ea4e4a7f72
shou-watanabe <watanabeshou1520@gmail.com>
2021-12-22 20:26:25 +0900

    :sparkles: #49 logにlatency実装

--- src/core/logger/logger.go
+++ src/core/logger/logger.go
@@ -2,16 +2,19 @@ package logger
 
 import (
 	"net/http"
+	"time"
 
 	"go.uber.org/zap"
 )
 
-func HttpLogging(msg string, r *http.Request) {
+func HttpLogging(msg string, r *http.Request, start time.Time, latency int64) {
 	logger, _ := zap.NewProduction()
 	logger.Info(
 		msg,
 		zap.String("method", r.Method),
 		zap.String("host", r.Host),
 		zap.String("path", r.URL.Path),
+		zap.Time("start", start),
+		zap.Int64("latency", latency),
 	)
 }
--- src/presen/middleware/logger.go
+++ src/presen/middleware/logger.go
@@ -2,14 +2,18 @@ package middleware
 
 import (
 	"net/http"
+	"time"
 
 	"techtrain-mission/src/core/logger"
 )
 
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		logger.HttpLogging("incoming request", r)
-
+		start := time.Now()
 		next.ServeHTTP(rw, r)
+		end := time.Now()
+		sub := end.Sub(start)
+		latency := int64(sub / time.Millisecond)
+		logger.HttpLogging("incoming request", r, start, latency)
 	})
 }
