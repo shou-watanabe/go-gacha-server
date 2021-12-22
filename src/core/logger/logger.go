e564a3a3e9cc768d2f275777c49328d5c826b410
shou-watanabe <watanabeshou1520@gmail.com>
2021-12-22 20:27:08 +0900

    :sparkles: #49 OS情報を取得

--- src/core/logger/logger.go
+++ src/core/logger/logger.go
@@ -5,12 +5,15 @@ import (
 	"time"
 
 	"go.uber.org/zap"
+
+	"techtrain-mission/src/core/context"
 )
 
 func HttpLogging(msg string, r *http.Request, start time.Time, latency int64) {
 	logger, _ := zap.NewProduction()
 	logger.Info(
 		msg,
+		zap.String("OS", r.Context().Value(context.OsKey).(string)),
 		zap.String("method", r.Method),
 		zap.String("host", r.Host),
 		zap.String("path", r.URL.Path),
